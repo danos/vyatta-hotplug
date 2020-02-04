d477ff811fc61be5669b795102df1b67b1ceedce
Paul Atkins <paul.atkins@intl.att.com>
2020-02-04 09:32:17 +0000

    vmxnet3: remove the special case rule for vmxnet3
    There was a hotplug rule for vmxnet3 that forced an unbind/rebind
    of the vmxnet3 interfaces as sometimes they came up with uio as the
    driver instead of vmxnet3. Depending on the timing of this (with
    respect to dataplane start) it could cause it to look like a hotplug
    reattach, and mess up the state for the interface. Remove this
    workaround as it is no longer needed.
    
    VRVDR-49420

--- scripts/vyatta-udev.sh
+++ scripts/vyatta-udev.sh
@@ -6,28 +6,8 @@ PCISLOT=$(echo "$DEVPATH" | awk -F/ '{for(i = NF; i ; i--) { if ($i~/:.*:.*\./) 
 socket="/var/run/vplane.socket"
 if [ -S $socket ];
 then
-    if [ "$SUBSYSTEM" = "uio" ]; then
-	# We are only called for uio device add for vmware vmxnet3 nic. This
-	# is to workaround a problem where the device initially appears bound
-	# to the uio pci module (eg. uio_pci_generic) rather than the vmxnet3
-	# kernel driver. In this case rebind it to vmxnet3 which will cause
-	# the event we expected in the first place, a net device add. We then
-	# proceed as normal.
-	if [ -e "/var/run/vyatta/hotplug/$PCISLOT" ]; then
-	    rm "/var/run/vyatta/hotplug/$PCISLOT"
-	else
-	    echo "$PCISLOT" > "/sys/bus/pci/drivers/$1/unbind"
-	    echo "$PCISLOT" > "/sys/bus/pci/drivers/vmxnet3/bind"
-	fi
-        exit 0
-    fi
-
     if [ "$ACTION" = "add" ]; then
 	/lib/vplane/vplane-uio "$PCISLOT"
-	if [ "$1" = "VMXNET3" ]; then
-	    mkdir -p /var/run/vyatta/hotplug
-	    touch "/var/run/vyatta/hotplug/$PCISLOT"
-        fi
     fi
     /opt/vyatta/sbin/vyatta-hotplug.pl --act="$ACTION" --pcislot="$PCISLOT"
 fi
